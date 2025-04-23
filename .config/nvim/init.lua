8027cf84f15b51c1371e66521355c03115103f9e
madsnkr <mrmadsnk@hotmail.com>
2025-04-23 12:27:05 +0200

    Add nvim mode switch
    This enables us to switch between using the minimal or the LazyVim/IDE
    config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+local ide_mode = os.getenv("NVIM_MODE") == "1"
+
+if ide_mode then
+	-- bootstrap lazy.nvim, LazyVim and your plugins
+	require("config.lazy")
+else
+	vim.cmd("source ~/.config/nvim/minimal.vim")
+end
